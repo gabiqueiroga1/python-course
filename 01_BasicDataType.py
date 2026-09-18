2bf13bb86ff764c4bbfcf4cd0f89b78eeea4ef31
gabiqueiroga1 <gabriela.queiroga1603@gmail.com>
2026-09-18 18:57:00 -0300

    feat: adiciona exemplos de tipos de dados

---
+++ 01_BasicDataType.py
@@ -0,0 +1,68 @@
+# camelCase
+# PascalCase
+# snake_case
+
+# Text Variables
+text = 'Tex in python is called String'
+text_2 = "It has to be inside single or double quotes"
+text_3 = 'We can use "double-quotes" inside single quotes'
+text_4 = "And 'vice-versa' " # This is string
+
+# Print Statements
+
+print(text)
+print(text_2)
+print(text_3)
+print(text_4)
+
+# Special Keywords in Python (Do not use)
+
+# print
+# type
+# sum
+# filter
+# id
+
+# not
+# in
+# break
+# continue
+# for
+# if
+# elif
+# else
+
+# str
+# float
+# list
+# bool
+# int
+
+# Basis Data-Types in Python
+
+string = 'text'  
+str_number = '10'   # string
+num_int = 10        # inteiro
+num_float = 3.14    # cdecimal
+boolean = True      # True or False
+none_type = None    # Nada  
+
+
+# print(type(string))
+# print(type(str_number))
+# print(type(num_int))
+# print(type(num_float))
+# print(type(boolean))
+# print(type(none_type))
+
+# Simple Excercise
+
+user = 'Igor'
+app = 'Java'
+
+print(f'Once upon a time, there was a {app} User named {user}')
+print(f'{user} wasted months on soul-draining tasks in {app}')
+print(f'Until one day {user} said: Enough!')
+print(f'And by accident he discovered that he could automate his {app} work with')
+print(f'But {user} knew nothing about {app}')
+print(f'And so, {user} has begun his programming journey.')
