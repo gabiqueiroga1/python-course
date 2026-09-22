f683578cdce052cccb823bf6e66cccbadc09aa1b
gabiqueiroga1 <gabriela.queiroga1603@gmail.com>
2026-09-22 16:16:08 -0300

    praticando exercicios de manipulacoes de string

---
+++ exercicios/manipulacoes_string.py
@@ -0,0 +1,26 @@
+# 1 exercicio (nome completo string)
+
+# nome = "Gabriela"
+# sobrenome = "Queiroga"
+
+# print("Nome completo: ",nome + " " + sobrenome)
+
+# # ou
+
+# nome_completo = nome + " " + sobrenome
+# print(nome_completo)
+
+# # nome completo em maiúsculo
+# print(nome_completo.upper())
+
+# exercicio 2
+
+frase = "não é um bug, é uma funcionalidade não documentada."
+
+print(frase)                                     # frase inteira
+print("total de caracteres: ",len(frase))       # quantidade de caracteres da frase
+print(frase.upper())                             # frase maiuscula
+print(frase.lower())                             # frase minuscula
+print(frase.capitalize())                        # a primeira letra maiuscula
+
+
