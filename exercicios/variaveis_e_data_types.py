fe458a897ba743e85220caaf92fffc009cc616a7
gabiqueiroga1 <gabriela.queiroga1603@gmail.com>
2026-09-22 15:59:31 -0300

    feat: adiciona arquivo para prática de data types em python

---
+++ exercicios/variaveis_e_data_types.py
@@ -0,0 +1,42 @@
+# exercício 1
+# criar variaveeis para armazenar nome, idade, altura, esta usando python(True or False)
+
+# nome = "gabriela"
+# idade = 20
+# altura = 1.65
+# programa_em_python = True
+
+# print(nome)
+# print(idade)
+# print(altura)
+# print(programa_em_python)
+
+# exercicio 2 (calculadora simples)
+
+# num1 = 5
+# num2 = 10
+
+# soma = num1 + num2
+# subtracao = num1 - num2
+# multiplicacao = num1 * num2
+# divisao = num1 / num2 
+
+# print(soma, subtracao, multiplicacao, divisao)
+
+# ou
+
+# print(f"resultado soma: {num1 + num2} ")
+# print(f"resultado subtracãoo: {num1 - num2} ")
+# print(f"resultado multiplicação: {num1 * num2} ")
+# print(f"resultado divisão: {num1 / num2} ")
+
+# exercicio 3 (conversao de tipos)
+#transforme idade "20" em inteiro e faça uma operação matematica com ela
+idade_str = "20"
+
+idade_int = int(idade_str)
+operacao = idade_int**2
+
+print(idade_str)
+print(idade_int)
+print(operacao)
