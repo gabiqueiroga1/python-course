1efd7d9c60e172f265d78cb9ab270f74edf197cd
gabiqueiroga1 <gabriela.queiroga1603@gmail.com>
2026-09-22 16:49:33 -0300

    feat: praticando manipulacoes lista em python

---
+++ exercicios/manipulacoes_lista.py
@@ -0,0 +1,26 @@
+# exercicio 1 
+
+# frutas = ['Uva', 'Pera', 'Morango', 'Tangerina', 'Manga']   # lista de frutas
+
+# print(frutas)                       # mostra lista
+# print(frutas[0])                    # primeiro item
+# print(frutas[-1])                   # ultimo item
+# print(frutas[4])                    # ultimo item tbm
+# frutas.append('Goiaba')             # adiciona nova fruta
+# frutas.remove('Uva')                # remove uva
+# print(frutas)                       # lista final
+
+
+# exercicio 2
+
+notas = [7, 8.5, 6, 9, 10]
+
+print(notas)
+print(notas[0])
+print(notas[-1])
+notas.append(5)
+notas.remove(7)
+print(notas)
+
+media = sum(notas) / len(notas)
+print(media)
